fa06b8219baaa21980175df91d8181a0249e86a9
rrybatc <r.rybachok@gmail.com>
2021-11-30 23:49:13 +0200

    routers added; entities added; handlers added

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-	api "github.com/romanrybachok/testapi"
+	"github.com/romanrybachok/testapi"
+	"github.com/romanrybachok/testapi/pkg/handler"
 	"log"
 )
 
 func main() {
+	handlers := new(handler.Handler)
+
 	srv := new(api.Server)
 
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server %s", err.Error())
 	}
 }
