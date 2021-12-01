9eaa37ca67781c49e73ac09145b8250e7eb446c9
rrybatc <r.rybachok@gmail.com>
2021-12-01 04:20:05 +0200

    config added; migrations added

--- cmd/main.go
+++ cmd/main.go
@@ -5,17 +5,29 @@ import (
 	"github.com/romanrybachok/testapi/pkg/handler"
 	"github.com/romanrybachok/testapi/pkg/repository"
 	"github.com/romanrybachok/testapi/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("Error initializing config %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(api.Server)
 
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+
+	return viper.ReadInConfig()
 }
